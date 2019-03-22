d424478e051e366159784f58f407e98c47b1b572
dominicho97 <dominic.ho97@gmail.com>
2019-03-22 11:07:43 +0100

    added property data (6) and methods setNewProperty,getProperty

--- includes/newclass.inc.php
+++ includes/newclass.inc.php
@@ -5,7 +5,13 @@ class NewClass {
 
 //properties andd methods goes here
 
+    public $data = "I am a property";
+    public function setNewProperty($newdata){
+    $this->data = "$newdata";
+  }
 
-
+    public function getProperty(){
+      return $this->data;
+  }
 }
 
